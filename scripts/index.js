109a94d4bc7b300668c4532daa6c49023cae54e5
Nikita Ershov <vohskin@gmail.com>
2021-04-16 05:02:29 +0300

    start work

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,21 @@ let closeButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__info-name');
 let profileAbout = document.querySelector('.profile__info-about');
 let popupForm = document.querySelector('.popup__form');
+let popupContainer = document.querySelector('.popup__container');
 
 
-function popupOpen() {
+function popupOpen(popupType) {
   popup.classList.add("popup_opened");
+  popupContainer.classList.add("popup__container_opened");
 
-  inputName.value = profileName.textContent
-  inputAbout.value = profileAbout.textContent
-  // console.log(popup.classList)
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
 
 
 function popupClosed() {
   popup.classList.remove("popup_opened");
+  popupContainer.classList.remove("popup__container_opened");
 }
 
 
